903f9d45016485f3b36868ce5e536d90b948d780
Elijah <kimelijah90@gmail.com>
2014-11-24 14:07:04 -0500

    Initial Commit
    * Create Rails using suspenders
    * Install Monban
    * Create Users

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+require "monban/constraints/signed_in"
+require "monban/constraints/signed_out"
+
+Rails.application.routes.draw do
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+
+  constraints Monban::Constraints::SignedIn.new do
+    root "dashboards#show", as: :dashboard
+  end
+
+  constraints Monban::Constraints::SignedOut.new do
+    root "sessions#new"
+  end
+end
