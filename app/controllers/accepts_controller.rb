61b230cfa90ac7b8c6d0db0f2d641878546258e0
Elijah <kimelijah90@gmail.com>
2014-12-01 11:16:50 -0500

    refactored

--- app/controllers/accepts_controller.rb
+++ app/controllers/accepts_controller.rb
@@ -1,7 +1,7 @@
 class AcceptsController < ApplicationController
   def create
     @match_request = MatchRequest.find(params[:match_request_id])
-    @match_request.update(confirmed: true)
+    @match_request.confirm
     @match_request.save
 
     redirect_to :back
