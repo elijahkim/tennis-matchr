42c2fbdc9dd6b3d0d87e5d28c9b2a13c15bd14c7
Elijah Kim <Eli@Elijahs-MacBook-Pro.local>
2014-12-04 17:42:05 -0500

    Add Match Show Page
    * Match show page shows challenger and defender
    * Match show page shows time
    * Match link from dashboard
    * Add match partial

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,7 @@
 class Match < ActiveRecord::Base
   belongs_to :challenger, class_name: "User"
   belongs_to :defender, class_name: "User"
+
+  delegate :username, to: :challenger, prefix: true
+  delegate :username, to: :defender, prefix: true
 end
