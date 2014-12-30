d4fc29e5882bd71d7a6fa6e7e64c5496461531b8
Elijah Kim <kimelijah90@gmail.com>
2014-12-30 10:10:21 -0500

    Change Match Request Logic
    * Only a match_at date is needed
    * A player can edit the match_date
    * Add some tests
    * Add a validator to make sure a user doesn't request self
    * Remove old validators
    * Remove start_date and end_date from match_request model

--- spec/models/match_request_spec.rb
+++ spec/models/match_request_spec.rb
@@ -2,6 +2,8 @@ require "rails_helper"
 
 RSpec.describe MatchRequest, type: :model do
   it { is_expected.to validate_presence_of(:request_message) }
+  it { is_expected.to validate_presence_of(:opponent) }
+  it { is_expected.to validate_presence_of(:requester) }
 
   it { is_expected.to belong_to(:requester).class_name("User") }
   it { is_expected.to belong_to(:opponent).class_name("User") }
@@ -12,6 +14,29 @@ RSpec.describe MatchRequest, type: :model do
       match_request = build(:match_request)
       match_request.confirm!
       expect(match_request).to be_confirmed
+    end
+  end
+
+  describe ".pending" do
+    it "returns all match requests that have not been accepted" do
+      match_request_one = build(:match_request)
+      match_request_one.confirm!
+
+      match_request_two = build(:match_request)
+
+      expect(pending).to eq(match_request_two)
+    end
+  end
+
+  describe "#get_comments" do
+    it "returns all the Match Requests comments" do
+      pending("Not sure how to test")
+    end
+  end
+
+  describe "#player?(user)" do
+    it "checks to see if a user is a requester or opponent" do
+      pending("Not sure how to test")
     end
   end
 end
